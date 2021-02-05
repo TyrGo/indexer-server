86bf3305dc7ae4aa48b7f2a44bdf5ed3887e6679
Ty <tyron.golds@gmail.com>
2021-02-04 20:50:27 -0500

    testing email

--- app.py
+++ app.py
@@ -13,11 +13,8 @@ CORS(app)
 
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
-    # MAIL_PORT=465,
-    # MAIL_USE_SSL=True,
-    MAIL_PORT = 465,
-    MAIL_USE_TLS = True,
-    MAIL_USE_SSL = True,   
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,  
     MAIL_USERNAME='getyourindex@gmail.com',
     MAIL_PASSWORD=PASSWORD
 )
