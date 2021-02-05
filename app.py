1f4f004a6f477d2e88ddaa0a69be6f6e8ba5ed96
Ty <tyron.golds@gmail.com>
2021-02-04 22:23:24 -0500

    remove print statement

--- app.py
+++ app.py
@@ -39,7 +39,6 @@ def send_index(email):
 
     with app.open_resource("index.txt") as fp:
         msg.attach("index.txt", "text/plain", fp.read())
-    print("p******", PASSWORD[:4])
 
     mail.send(msg)
     os.remove("index.txt")
