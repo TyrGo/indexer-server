89a5195bf9fa34c48f76dbabfe6d36164c1e31a3
Ty <tyron.golds@gmail.com>
2021-02-18 21:52:42 -0500

    S3 update

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from rq import Queue
 # from config import PASSWORD, SECRET_KEY, S3_BUCKET, S3_KEY, S3_SECRET
 SECRET_KEY = os.environ.get('SECRET_KEY')
 PASSWORD = os.environ.get('PASSWORD')
-S3_BUCKET = os.environ.get('S3_BUCKET_NAME')
+S3_BUCKET = os.environ.get('S3_BUCKET')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
